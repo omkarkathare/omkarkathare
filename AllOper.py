e3dfb9363bf9fb06a9b47a60b18915640bb05b89
omkarkathare <97381416+omkarkathare@users.noreply.github.com>
2022-01-09 21:19:46 +0530

    Add files via upload

---
+++ AllOper.py
@@ -0,0 +1,56 @@
+p,q,r = 5,15,25
+
+# Arithmetic Operator
+print("\n 5 + 15 = ",p+q)
+
+print("\n 25 - 15 = ",r-q)
+
+print("\n 5 * 15 = ",p*q)
+
+print("\n 25 / 5 = ",r/q)
+
+# Comparison Operator
+x,y = 11,22
+print("\n x < y",x<y)
+
+print("\n x > y",x>y)
+
+print("\n x <= y",x<=y)
+
+print("\n x >= y",x>=y)
+
+print("\n x == y",x==y)
+
+print("\n x != y",x!=y)
+
+# Logical Operator
+x = True
+y = False
+
+print("x and y is",x & y)
+
+print("x or y is",x or y)
+
+print("not y is",not y)
+
+# Identity Operator
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+
+print("x1 is not y1 ",x1 is not y1)
+
+print("x2 is y2",x2 is y2)
+
+# Membership Operator
+x = 'Hello Rohan'
+y = {1:'a',2:'b'}
+
+print("H in x",'H' in x)
+
+print("hello not in x",'hello' not in x)
+
+print("1 in y",1 in y)
+
+print("a in y",'a' in y)
